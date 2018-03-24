ff54acbf5f905c621a596a2480e4ff331a9978d7
Audumbar Nevarekar <audumbarnevarekar@gmail.com>
2018-03-25 00:08:04 +0530

    Initial commit with base packages

---
+++ src/test/java/com/cricket/cricketscorerapp/CricketScorerAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cricket.cricketscorerapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CricketScorerAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
