2cd21754201883f7ebd91090a4c84a7e4d3f44e2
Audumbar Nevarekar <audumbarnevarekar@gmail.com>
2018-03-29 00:03:12 +0530

    Added BowlingStats & BattingStats for a player

---
+++ src/main/java/com/cricket/cricketscorerapp/stats/pojo/BattingStats.java
@@ -0,0 +1,77 @@
+package com.cricket.cricketscorerapp.stats.pojo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name="csa_tbl_batting_stats")
+public class BattingStats {
+	
+	@Id
+	@GeneratedValue(generator="system-uuid")
+	@GenericGenerator(name="system-uuid", strategy="uuid2")
+	private String battingStatsId;
+	
+	private int inningsPlayed;
+	private int notouts;
+	private int runs;
+	private int highestScore;
+	private int ballsFaced;
+	private int fours;
+	private int sixes;
+	public String getBattingStatsId() {
+		return battingStatsId;
+	}
+	public void setBattingStatsId(String battingStatsId) {
+		this.battingStatsId = battingStatsId;
+	}
+	public int getInningsPlayed() {
+		return inningsPlayed;
+	}
+	public void setInningsPlayed(int inningsPlayed) {
+		this.inningsPlayed = inningsPlayed;
+	}
+	public int getNotouts() {
+		return notouts;
+	}
+	public void setNotouts(int notouts) {
+		this.notouts = notouts;
+	}
+	public int getRuns() {
+		return runs;
+	}
+	public void setRuns(int runs) {
+		this.runs = runs;
+	}
+	public int getHighestScore() {
+		return highestScore;
+	}
+	public void setHighestScore(int highestScore) {
+		this.highestScore = highestScore;
+	}
+	public int getBallsFaced() {
+		return ballsFaced;
+	}
+	public void setBallsFaced(int ballsFaced) {
+		this.ballsFaced = ballsFaced;
+	}
+	public int getFours() {
+		return fours;
+	}
+	public void setFours(int fours) {
+		this.fours = fours;
+	}
+	public int getSixes() {
+		return sixes;
+	}
+	public void setSixes(int sixes) {
+		this.sixes = sixes;
+	}
+	
+}
---
+++ src/main/java/com/cricket/cricketscorerapp/stats/pojo/BowlingStats.java
@@ -0,0 +1,55 @@
+package com.cricket.cricketscorerapp.stats.pojo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name="csa_tbl_bowling_stats")
+public class BowlingStats {
+	@Id
+	@GeneratedValue(generator="system-uuid")
+	@GenericGenerator(name="system-uuid", strategy="uuid2")
+	private String bowlingStatsId;
+	
+	private int inningsPlayed;
+	private int deliveries;
+	private int runsGiven;
+	private int wicketsTaken;
+	
+	public String getBowlingStatsId() {
+		return bowlingStatsId;
+	}
+	public void setBowlingStatsId(String bowlingStatsId) {
+		this.bowlingStatsId = bowlingStatsId;
+	}
+	public int getInningsPlayed() {
+		return inningsPlayed;
+	}
+	public void setInningsPlayed(int inningsPlayed) {
+		this.inningsPlayed = inningsPlayed;
+	}
+	public int getDeliveries() {
+		return deliveries;
+	}
+	public void setDeliveries(int deliveries) {
+		this.deliveries = deliveries;
+	}
+	public int getRunsGiven() {
+		return runsGiven;
+	}
+	public void setRunsGiven(int runsGiven) {
+		this.runsGiven = runsGiven;
+	}
+	public int getWicketsTaken() {
+		return wicketsTaken;
+	}
+	public void setWicketsTaken(int wicketsTaken) {
+		this.wicketsTaken = wicketsTaken;
+	}
+	
+}
