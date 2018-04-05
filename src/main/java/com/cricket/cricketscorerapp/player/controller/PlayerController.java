740f895b74055451813e81fda505d217b8b8874f
Audumbar Nevarekar <audumbarnevarekar@gmail.com>
2018-04-05 23:26:38 +0530

    Added @CrossOrigin annotation

--- src/main/java/com/cricket/cricketscorerapp/player/controller/PlayerController.java
+++ src/main/java/com/cricket/cricketscorerapp/player/controller/PlayerController.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import com.cricket.cricketscorerapp.player.service.PlayerService;
 
 @RestController
 @RequestMapping("/players")
+@CrossOrigin
 public class PlayerController {
 	@Autowired
 	PlayerService playerService;
--- src/main/java/com/cricket/cricketscorerapp/tournament/controller/TournamentController.java
+++ src/main/java/com/cricket/cricketscorerapp/tournament/controller/TournamentController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ import com.cricket.cricketscorerapp.tournament.service.TournamentService;
  */
 @RestController
 @RequestMapping("/tournament")
+@CrossOrigin
 public class TournamentController {
 
 	@Autowired
