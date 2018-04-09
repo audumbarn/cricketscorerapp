5dab261a583264d8a464436741fbe0958b38cf4f
Audumbar Nevarekar <audumbarnevarekar@gmail.com>
2018-04-09 23:03:51 +0530

    Added exception message constants class

---
+++ src/main/java/com/cricket/cricketscorerapp/util/ExceptionMessageConstants.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.cricket.cricketscorerapp.util;
+
+/**
+ * @author Audumbar Nevarekar
+ *
+ */
+public class ExceptionMessageConstants {
+
+	public static final String INNING_NOT_FOUND_EXCEPTION_MSG = "Inning Not Found";
+}
