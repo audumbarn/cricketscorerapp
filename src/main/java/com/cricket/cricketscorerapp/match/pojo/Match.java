a57fbbb22f80080ffbc24441589c2eb12c1c3f14
Audumbar Nevarekar <audumbarnevarekar@gmail.com>
2018-04-11 22:23:31 +0530

    Revert "removed tournamentId"
    This reverts commit b6a0719e48697380170672481a8765dcfe49ee23.

--- src/main/java/com/cricket/cricketscorerapp/match/pojo/Match.java
+++ src/main/java/com/cricket/cricketscorerapp/match/pojo/Match.java
@@ -31,6 +31,7 @@ public class Match {
 	private String matchWonBy;
 	private String venue;
 	private int matchStatus;
+	private String tournamentId;
 	private String matchLabel;
 	private String matchGroup;
 	
@@ -97,6 +98,12 @@ public class Match {
 	}
 	public void setMatchStatus(int matchStatus) {
 		this.matchStatus = matchStatus;
+	}
+	public String getTournamentId() {
+		return tournamentId;
+	}
+	public void setTournamentId(String tournamentId) {
+		this.tournamentId = tournamentId;
 	}
 	public String getMatchLabel() {
 		return matchLabel;
