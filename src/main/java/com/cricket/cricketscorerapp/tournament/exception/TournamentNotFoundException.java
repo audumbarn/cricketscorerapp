8ab7ed6174fd72e9b89548819e23d71eaf8eb097
Audumbar Nevarekar <audumbarnevarekar@gmail.com>
2018-04-14 23:11:30 +0530

    removed @ResponseStatus

--- src/main/java/com/cricket/cricketscorerapp/tournament/exception/TournamentNotFoundException.java
+++ src/main/java/com/cricket/cricketscorerapp/tournament/exception/TournamentNotFoundException.java
@@ -3,16 +3,12 @@
  */
 package com.cricket.cricketscorerapp.tournament.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 import com.cricket.cricketscorerapp.util.ExceptionMessageConstants;
 
 /**
  * @author Audumbar Nevarekar
  *
  */
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class TournamentNotFoundException extends RuntimeException{
 	
 	public TournamentNotFoundException() {
